8a3d17df9d3716461976c069a94b2d291d7024a7
Calvin Ellis <gumdum@calvinellis.dev>
2022-01-12 09:24:26 -0600

    First commit - skeleton lib

---
+++ example/dart_keycloak_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_keycloak_api/dart_keycloak_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_keycloak_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_keycloak_api;
+
+export 'src/dart_keycloak_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
